67afac82c94ba8e88a927b4108b96d775cfc58a4
ST10449855 <163865994+ST10449855@users.noreply.github.com>
2024-10-08 01:07:48 +0200

    Done

--- App.tsx
+++ App.tsx
@@ -1,20 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <SafeAreaView style={styles.container}>
+      <View style={styles.headingContainer}>
+        <Text style={styles.container}>The Menu</Text>
+      </View>
+
+    <View style={styles.listView}>
+
     </View>
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    padding: 20,
+    marginVertical:1,
+    backgroundColor: 'white',
+  },
+  headingContainer:{
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+    marginBottom: 100,
+    width: '100%'
+  },
+  trackerName:{
+    fontSize: 34,
+    fontWeight: 'bold',
+  },
+  listView:{
+    marginTop: -60,
+    width: '100%',
+    height: 550,
+    borderRadius:10,
+    backgroundColor: 'purple',
+  },
+  userImageView:{
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    width: '100%',
+    marginVertical: 5,
+    backgroundColor:'',
   },
 });
