4d9ee59e9987bbef04b0e9f0fa55d38a6f284bee
ST10449855 <163865994+ST10449855@users.noreply.github.com>
2024-10-08 08:11:32 +0200

    Some changes made

--- App.tsx
+++ App.tsx
@@ -1,7 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import {FlatList, SafeAreaView, StyleSheet, Text,TextInput,TouchableHighlight, View } from 'react-native';
+import {Picker} from "@react-native-picker/picker";
+import { WorkoutDetail } from './type';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [workOutName, setWorkOutName] = useState<string>('');
+  const [duration, setDuration] = useState<string>('');
+  const [excerciseType, setExerciseType] = useState<string>('');
+  const [calories, setCalories] = useState<string>('');
+
+  const[workouts, setWorkouts] = useState<WorkoutDetail[]>([]);
+
+  const ExcerciseType = ['Çardio','Strength','Felxibilty','Balance','HIIT']
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.headingContainer}>
@@ -12,10 +24,31 @@ export default function App() {
 
     </View>
 
+    <View style={styles.userInputView}>
+      <TextInput style={styles.input} placeholder="Workout name" value={workOutName} onChangeText={setWorkOutName}></TextInput>  
+
+      <TextInput style={styles.input} placeholder="Duration (min)" value={duration} onChangeText={setDuration}></TextInput>
+    
+    <Picker style={styles.input}
+    selectedValue={excerciseType}
+    onValueChange={(itemValue) => setExerciseType(itemValue)}>
+    
+    {ExcerciseType.map((excerciseType) => (
+      <Picker.Item label={excerciseType} value={excerciseType} key={excerciseType}/>
+    ))}
+      </Picker>
+  
+      <TextInput style={styles.input} placeholder="Calories"></TextInput>
+
+      <TouchableHighlight style={styles.button}>
+        <Text style={styles.buttonText}>Save</Text>
+      </TouchableHighlight>
+    </View>
+
     </SafeAreaView>
+
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -42,7 +75,7 @@ const styles = StyleSheet.create({
     borderRadius:10,
     backgroundColor: '#ECECEC',
   },
-  userImageView:{
+  userInputView:{
     alignItems: 'center',
     justifyContent: 'flex-start',
     width: '100%',
@@ -51,6 +84,31 @@ const styles = StyleSheet.create({
     padding: 15,
     marginTop: 40,
     marginBottom: 50,
-    borderRadius:10
+    borderRadius:10,
   },
+  input:{
+    width:'100%',
+    height: 40,
+    backgroundColor:'white',
+    paddingHorizontal: 10,
+    marginVertical: -15,
+    borderRadius: 5,
+    color: 'black',
+    marginTop: 30,
+    fontSize: 20,                                                                                                                      
+  },
+  button:{
+    backgroundColor:'#fff',
+    paddingVertical: 15,
+    paddingHorizontal: 40,
+    borderRadius: 40, // for roundede corner
+    marginVertical: 10,
+    alignItems: 'center',
+    marginTop: 40,
+  },
+  buttonText:{
+    color: 'black',
+    fontSize: 20,
+    fontWeight: 'bold',
+  }
 });
---
+++ type.tsx
@@ -0,0 +1,6 @@
+export type WorkoutDetail = {
+    workout_Name: string;
+    duration: number;
+    excercise_Type: string;
+    calories: number;
+}
